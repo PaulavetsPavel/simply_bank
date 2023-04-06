4947b168c7804da5db80ffe9dadd54c92ae09a44
Pavel Paulavets <paulavetspavel@outlook.com>
2023-04-06 15:07:19 +0300

    Add animation for appearance of content & refactoring stiky nav

--- script.js
+++ script.js
@@ -6,8 +6,10 @@ const btnCloseModalWindow = document.querySelector('.btn--close-modal-window');
 const btnsOpenModalWindow = document.querySelectorAll(
 	'.btn--show-modal-window',
 );
+const body = document.querySelector('body');
 const header = document.querySelector('.header');
 const btnScrollTo = document.querySelector('.btn--scroll-to');
+const sections = document.querySelectorAll('section');
 const section1 = document.querySelector('#section--1');
 
 const nav = document.querySelector('.nav');
@@ -18,94 +20,57 @@ const operationsTabContents = document.querySelectorAll('.operations__content');
 const operationsTabContainer = document.querySelector(
 	'.operations__tab-container',
 );
-
 //==================================================
 
-//          Modal window
-const openModalWindow = (e) => {
-	e.preventDefault();
-	modalWindow.classList.remove('hidden');
-	overlay.classList.remove('hidden');
+//*        Appearance section after scroll
+const appearanceSection = (entries, observer) => {
+	// entries = sectionObserver.threshold
+	const entry = entries[0];
+	if (!entry.isIntersecting) return;
+	entry.target.classList.remove('section--hidden');
+	observer.unobserve(entry.target);
 };
-//-----------------------------------------------------
-const closeModalWindow = (e) => {
-	e.preventDefault();
-	modalWindow.classList.add('hidden');
-	overlay.classList.add('hidden');
+const sectionObserver = new IntersectionObserver(appearanceSection, {
+	// element or null (viewport)
+	root: null,
+	// [] with % visibility of element from .observ()
+	threshold: 0.1,
+});
+sections.forEach((section) => {
+	// Hide sections but header
+	section.classList.add('section--hidden');
+	sectionObserver.observe(section);
+});
+//==================================================
+
+//*         Sticky navigation
+// Intersection Observer API
+const getStikyNav = (entries, observer) => {
+	const entry = entries[0];
+	const method = !entry.isIntersecting ? 'add' : 'remove';
+	nav.classList[method]('sticky');
 };
-// Add function open modal window for every button
-btnsOpenModalWindow.forEach((btn) => {
-	btn.addEventListener('click', openModalWindow);
+const navHeight = nav.getBoundingClientRect().height;
+const headerObserver = new IntersectionObserver(getStikyNav, {
+	root: null,
+	threshold: 0,
+	// before the end of the section
+	rootMargin: `-${navHeight}px`,
 });
-
-btnCloseModalWindow.addEventListener('click', closeModalWindow);
-overlay.addEventListener('click', closeModalWindow);
-document.addEventListener('keydown', (e) => {
-	if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
-		closeModalWindow(e);
-	}
-});
+headerObserver.observe(header);
+//--------------------------------------------------
+// Method using coordinates
+// const section1Coordinates = section1.getBoundingClientRect();
+// window.addEventListener('scroll', (e) => {
+// 	if (window.scrollY > section1Coordinates.top) {
+// 		nav.classList.add('sticky');
+// 	} else {
+// 		nav.classList.remove('sticky');
+// 	}
+// });
 //==================================================
 
-//       Cookie message
-// Create
-const cookieMessageElement = document.createElement('div');
-cookieMessageElement.classList.add('cookie-message');
-cookieMessageElement.innerHTML = `Мы используем на этом сайте cookie для улучшения функциональности.
-<button class='btn btn--close-cookie'>Ok!</button>`;
-header.append(cookieMessageElement);
-//-----------------------------------------------------
-// Add style
-cookieMessageElement.style.height =
-	Number.parseFloat(getComputedStyle(cookieMessageElement).height) + 30 + 'px';
-cookieMessageElement.style.width = '80%';
-cookieMessageElement.style.backgroundColor = '#076785';
-cookieMessageElement.style.borderRadius = '15px';
-//-----------------------------------------------------
-// Close
-document.querySelector('.btn--close-cookie').addEventListener('click', () => {
-	cookieMessageElement.remove();
-});
-// Get styles from class
-// console.log(getComputedStyle(cookieMessageElement));
-// console.log(getComputedStyle(cookieMessageElement).color);
-
-// Change of variable value in :root
-// document.documentElement.style.setProperty('variable','value')
-//==================================================
-
-//          Scroll to Section1 when press Know more
-btnScrollTo.addEventListener('click', (e) => {
-	e.preventDefault();
-	section1.scrollIntoView({ behavior: 'smooth' });
-
-	// Method for old browsers
-	// get coordinates of section1
-	// const section1Coordinates = section1.getBoundingClientRect();
-	// set new coodinates to show section1
-	// window.scrollTo({
-	// 	left: section1Coordinates.left,
-	// 	top: section1Coordinates.top + window.pageYOffset,
-	// 	behavior: 'smooth',
-	// });
-});
-//==================================================
-
-//          Smooth page navigation
-// Add event listener on parent
-navLinks.addEventListener('click', function (e) {
-	e.preventDefault();
-	console.log(e.target);
-	// Get attribute for cliked element if element is a link
-	if (e.target.classList.contains('nav__link')) {
-		const href = e.target.getAttribute('href');
-		// Smooth scrol to section
-		document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
-	}
-});
-//==================================================
-
-//          Animation for nav link
+//*          Animation for nav link
 const navLinksHoverAnimations = function (e) {
 	e.preventDefault();
 	if (e.target.classList.contains('nav__link')) {
@@ -130,25 +95,42 @@ nav.addEventListener('mouseout', navLinksHoverAnimations.bind(1));
 // nav.addEventListener('mouseover', (e) => {
 // 	navLinksHoverAnimations(e, 0.4);
 // });
-
 //==================================================
 
-//         Sticky navigation
-const section1Coordinates = section1.getBoundingClientRect();
-window.addEventListener('scroll', (e) => {
-	if (window.scrollY > section1Coordinates.top) {
-		nav.classList.add('sticky');
-	} else {
-		nav.classList.remove('sticky');
+//*         Scroll to Section1 when press Know more
+btnScrollTo.addEventListener('click', (e) => {
+	e.preventDefault();
+	section1.scrollIntoView({ behavior: 'smooth' });
+	// Method for old browsers
+	// get coordinates of section1
+	// const section1Coordinates = section1.getBoundingClientRect();
+	// set new coodinates to show section1
+	// window.scrollTo({
+	// 	left: section1Coordinates.left,
+	// 	top: section1Coordinates.top + window.pageYOffset,
+	// 	behavior: 'smooth',
+	// });
+});
+//==================================================
+
+//*          Smooth page navigation
+// Add event listener on parent
+navLinks.addEventListener('click', function (e) {
+	e.preventDefault();
+	console.log(e.target);
+	// Get attribute for cliked element if element is a link
+	if (e.target.classList.contains('nav__link')) {
+		const href = e.target.getAttribute('href');
+		// Smooth scrol to section
+		document.querySelector(href).scrollIntoView({ behavior: 'smooth' });
 	}
 });
 //==================================================
 
-//         Switching tabs
+//*         Switching tabs
 operationsTabContainer.addEventListener('click', (e) => {
 	e.preventDefault();
 	const clickedButton = e.target.closest('.operations__tab');
-
 	// If clickedButton === null exit from eventListener (click not a button)
 	if (!clickedButton) return;
 	// Active tab
@@ -164,4 +146,55 @@ operationsTabContainer.addEventListener('click', (e) => {
 		.querySelector(`.operations__content--${clickedButton.dataset.tab}`)
 		.classList.add('operations__content--active');
 });
+
+//==================================================
+//*       Cookie message
+// Create
+const cookieMessageElement = document.createElement('div');
+cookieMessageElement.classList.add('cookie-message');
+cookieMessageElement.innerHTML = `Мы используем на этом сайте cookie для улучшения функциональности.
+<button class='btn btn--close-cookie'>Ok!</button>`;
+body.append(cookieMessageElement);
+// Add style
+cookieMessageElement.style.height =
+	Number.parseFloat(getComputedStyle(cookieMessageElement).height) + 30 + 'px';
+cookieMessageElement.style.width = '80%';
+cookieMessageElement.style.backgroundColor = '#076785';
+cookieMessageElement.style.borderRadius = '15px';
+// Close
+document.querySelector('.btn--close-cookie').addEventListener('click', () => {
+	cookieMessageElement.remove();
+});
+// Get styles from class
+// console.log(getComputedStyle(cookieMessageElement));
+// console.log(getComputedStyle(cookieMessageElement).color);
+
+// Change of variable value in :root
+// document.documentElement.style.setProperty('variable','value')
+
+//==================================================
+//*        Modal window
+
+const openModalWindow = (e) => {
+	e.preventDefault();
+	modalWindow.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
+const closeModalWindow = (e) => {
+	e.preventDefault();
+	modalWindow.classList.add('hidden');
+	overlay.classList.add('hidden');
+};
+// Add function open modal window for every button
+btnsOpenModalWindow.forEach((btn) => {
+	btn.addEventListener('click', openModalWindow);
+});
+btnCloseModalWindow.addEventListener('click', closeModalWindow);
+overlay.addEventListener('click', closeModalWindow);
+document.addEventListener('keydown', (e) => {
+	if (e.key === 'Escape' && !modalWindow.classList.contains('hidden')) {
+		closeModalWindow(e);
+	}
+});
+//==================================================
 
